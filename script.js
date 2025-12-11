94de1698b5212784c38243fc88aabe09af5274c3
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-12-11 10:15:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+const radiusInput = document.getElementById('radius').value;
+	const radius = parseFloat(radiusInput);
+	let volume;
+
+	if (isNaN(radius) || radius < 0) {
+        volume = 'NaN'; 
+    } else {
+		const rawVolume = (4/3) * Math.PI * Math.pow(radius, 3);
+		volume = rawVolume.toFixed(4);
+    }
+	document.getElementById('volume').value = volume;
+	return false;
+}
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
